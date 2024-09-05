36bb476e03c3b0974f799e27dcc79976c5829420
devbykamal <kamalbajgain98@gmail.com>
2024-09-05 08:23:47 +0545

    last commit2

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ArrowBigLeftDashIcon, ArrowBigRightDashIcon, ChevronsRight } from "lucide-react";
+import { AppWindow, AreaChart, ArrowBigLeftDashIcon, ArrowBigRightDashIcon, ChevronsRight } from "lucide-react";
 import logo1 from "./img/logo1.png";
 import logo2 from "./img/logo2.png";
 import logo3 from "./img/logo3.png";
@@ -14,6 +14,7 @@ import products12 from "./img/products12.jpg";
 import products13 from "./img/products13.jpg";
 import products14 from "./img/products14.jpg";
 import products15 from "./img/products15.jpg";
+import app from "./img/app.png";
 import './components/dropright.css';
 
 function Products() {
@@ -67,29 +68,172 @@ function Products() {
   return (
     <>
       <div className="flex mx-4 sm:mx-10 gap-6 justify-between ">
-        <div className="category mt-7" id="all_select">
+
+        <div id="all_select" className="category max-lg:hidden">
           <div className="header">
-            <h3 className="category-header bg-[#843135] max-w-[220px] p-2 mb-1 box-border font-bold text-left">Categories</h3>
+            <h3 className="bg-[#843135] max-w-[240px] p-2 mb-1 box-border">Categories</h3>
           </div>
-          {["Light", "BUILDING MATERIALS", "TILES AND MARBLES", "HARDWARE", "INTERIOR", "CONSTRUCTION", "CONSTRUCTION MART"].map((category, i) => (
-            <div key={i} className="parent" style={{ textAlign: "left" }}>
-              {category}
-              <div className="child">
-                {[1, 2, 3].map((child) => (
-                  <div key={child} className="child_parent">
-                    Child {child}
-                    <div className="child_parent_child">
-                      {[1, 2, 3].map((grandchild) => (
-                        <div key={grandchild} className="child_item">
-                          Child's {child} Child {grandchild}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))}
+          <div className="parent text-left ">
+            <span className="flex justify-between"> Light <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">
+                <span className="flex justify-between"> KSD LIGHT <ArrowBigRightDashIcon /> </span>
+                <div className="child_parent_child">
+                  <div className="child_item">Child's Child 1</div>
+                  <div className="child_item">Child's Child 2</div>
+                  <div className="child_item">Child's Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                <span className="flex justify-between"> L & T <ArrowBigRightDashIcon /> </span>
+                <div className="child_parent_child">
+                  <div className="child_item">Child's Child 1</div>
+                  <div className="child_item">Child's Child 2</div>
+                  <div className="child_item">Child's Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                <span className="flex justify-between"> OTHER LIGHT <ArrowBigRightDashIcon /> </span>
+                <div className="child_parent_child">
+                  <div className="child_item">Child's Child 1</div>
+                  <div className="child_item">Child's Child 2</div>
+                  <div className="child_item">Child's Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                KSD FAN
+
+              </div>
+              <div className="child_parent">
+                KSD switch
+
               </div>
             </div>
-          ))}
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">  BUILDING MATERIALS <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">
+                Child 1
+                <div className="child_parent_child">
+                  <div className="child_item">Child's Child 1</div>
+                  <div className="child_item">Child's Child 2</div>
+                  <div className="child_item">Child's Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                Child 2
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 2 Child 1</div>
+                  <div className="child_item">Child's 2 Child 2</div>
+                  <div className="child_item">Child's 2 Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">        TILES AND MARBLES  <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">
+                Child 1
+                <div className="child_parent_child">
+                  <div className="child_item">Child's Child 1</div>
+                  <div className="child_item">Child's Child 2</div>
+                  <div className="child_item">Child's Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                Child 2
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 2 Child 1</div>
+                  <div className="child_item">Child's 2 Child 2</div>
+                  <div className="child_item">Child's 2 Child 3</div>
+                </div>
+              </div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">       HARDWARE <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">Child 1</div>
+              <div className="child_parent">Child 2</div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">   INTERIOR  <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">Child 1</div>
+              <div className="child_parent">Child 2</div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">         CONSTRUCTION  <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">Child 1</div>
+              <div className="child_parent">Child 2</div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="parent text-left">
+            <span className="flex justify-between">  CONSTRUCTION MART <ArrowBigRightDashIcon /> </span>
+            <div className="child">
+              <div className="child_parent">Child 1</div>
+              <div className="child_parent">Child 2</div>
+              <div className="child_parent">
+                Child 3
+                <div className="child_parent_child">
+                  <div className="child_item">Child's 3 Child 1</div>
+                  <div className="child_item">Child's 3 Child 2</div>
+                  <div className="child_item">Child's 3 Child 3</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="bg-white mt-7 flex flex-col p-6 sm:w-full max-w-auto mx-auto">
@@ -158,7 +302,7 @@ function Products() {
             <ChevronsRight className="ml-2 mt-1 transition-transform duration-300" />
           </a>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt p-4">
           {renderProduct(products11, "GH2525")}
           {renderProduct(products10, "GH2525")}
           {renderProduct(products12, "GH2525")}
@@ -171,6 +315,20 @@ function Products() {
           {renderProduct(products10, "GH2525")}
           {renderProduct(products11, "GH2525")}
           {renderProduct(products12, "GH2525")}
+        </div>
+      </div>
+      <div className='bg-white flex flex-col md:flex-row items-center md:items-start p-5 md:p-10 md:pb-0 mt-10'>
+        <img src={app} alt="App Preview" className='w-full max-w-[400px] h-auto' />
+        <div className='flex flex-col items-center md:items-start md:ml-10 text-center md:text-left'>
+          <h1 className='text-[20px] md:text-[21px] font-semibold mb-4 max-sm:mt-5 md:mt-32 px-4 md:px-0'>
+            Kshamadevi Group, Simplify your business <br />
+            | Order and Recommend our products
+          </h1>
+          <div className='p-4 md:p-5 bg-[#843135] text-white w-full max-w-[500px] rounded-md transition-all duration-300 ease-in-out hover:scale-105 md:mt-12'>
+            <h2 className='text-[18px] max-sm:text-[16px] flex items-center justify-center md:justify-start'>
+              Download The App Here:<AppWindow className='ml-2 mt-2 md:ml-3' />
+            </h2>
+          </div>
         </div>
       </div>
     </>
